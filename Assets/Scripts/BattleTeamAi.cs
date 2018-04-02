6b0e1e6de1f94a29ecbd140fd95f866e0094d531
loosepolygon <loosepolygon@gmail.com>
2018-04-02 13:23:20 -0700

    AI update now updates one mech at a time

--- Assets/Scripts/BattleTeamAi.cs
+++ Assets/Scripts/BattleTeamAi.cs
@@ -1,18 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class BattleTeamAi{
 	public BattleTeam ourTeam;
 
+	int mechUpdateIndex;
+
 	public BattleTeamAi(BattleTeam newTeam){
 		this.ourTeam = newTeam;
+
+		this.mechUpdateIndex = -1;
 	}
 
 	public void Update(){
-		foreach(BattleMech mech in this.ourTeam.mechs){
-			// For now, the commander's command is always to destroy the enemy mech
-			mech.ai.Update();
+		BattleMech currentMech = null;
+
+		// Select the next mech, or the first one with enough AP
+		int mechCount = this.ourTeam.mechs.Count;
+		for(int n = 0; n < mechCount; ++n){
+			this.mechUpdateIndex = (this.mechUpdateIndex + 1) % mechCount;
+			currentMech = this.ourTeam.mechs[this.mechUpdateIndex];
+			if(currentMech.actionPoints > 0){
+				break;
+			}
 		}
+		Assert.IsTrue(currentMech.actionPoints > 0);
+
+		currentMech.ai.Update();
 	}
 }
